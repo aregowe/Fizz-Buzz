13ce570adc8cb4b409dcd423f7f8ca0c31a1ee71
James Anderson <janderson133787@gmail.com>
2017-08-01 10:00:54 -0400

    Added fizzbuzz.jz
    Added fizzbuzz.jz

---
+++ fizzbuzz.js
@@ -0,0 +1,27 @@
+/*Return an array containing the numbers from 1 to N, where N is the parametered value. N will never be less than 1.
+
+Replace certain values however if any of the following conditions are met:
+
+    If the value is a multiple of 3: use the value 'Fizz' instead
+    If the value is a multiple of 5: use the value 'Buzz' instead
+    If the value is a multiple of 3 & 5: use the value 'FizzBuzz' instead
+*/
+
+function fizzbuzz(n)
+{
+  var i = 1, arr = [];
+  while(i <= n) {
+    var fizz = (i % 3 == 0);
+    var buzz = (i % 5 == 0);
+    if(fizz || buzz) {
+      arr.push((fizz?"Fizz":"") + (buzz?"Buzz":""));
+    }
+    else {
+      arr.push(i);
+    }
+    i++;
+  }
+  return arr;
+}
+
+var fizzify = fizzbuzz;
